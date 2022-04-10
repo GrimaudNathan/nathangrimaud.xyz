de4db61e49f1de34226c02c8c32f7396984ced17
Nathan <nathan.grimaud@epitech.eu>
2022-04-10 21:09:50 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['plugin:vue/vue3-essential', 'prettier'],
+  rules: {
+    // override/add rules settings here, such as:
+    'vue/no-unused-vars': 'error',
+    'vue/script-setup-uses-vars': 'off',
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ["Home", "Button", "Menu", "Trends"],
+      },
+    ],
+  },
+}
---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="relative mx-auto h-screen w-[60%]">
+    <div class="absolute top-1/2 -translate-y-1/2">
+      <h1 class="text-9xl font-bold text-white">Hello.</h1>
+      <h1 class="text-9xl font-bold text-blue-700">I am</h1>
+      <h1 class="text-9xl font-bold text-blue-700">Nathan</h1>
+    </div>
+  </div>
+</template>
